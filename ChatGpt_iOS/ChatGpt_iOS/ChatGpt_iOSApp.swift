002d0693eb511228197493abd5f78c2d3a527ae2
Allen Liu <liuj9@rose-hulman.edu>
2023-12-10 12:10:06 -0600

    add all projects

---
+++ ChatGpt_iOS/ChatGpt_iOS/ChatGpt_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGpt_iOSApp.swift
+//  ChatGpt_iOS
+//
+//  Created by Jingkun Liu on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGpt_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
